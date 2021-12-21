5915a2e2d44f529922b3b87923c3ff2dd9a9f803
Nikolay <terleew2013@yandex.ru>
2021-12-21 08:30:45 +0000

    feat: create productController

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product/{id}', name: 'product')]
+    public function index(ProductRepository $doctrine, $id): Response
+    {
+        $product = $doctrine->findOneBy(['id' => $id]);
+
+        return $this->render('product/index.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
