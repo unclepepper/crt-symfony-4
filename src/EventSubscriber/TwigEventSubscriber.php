0d41e7c660e2ca788d65eb88121c9ca825f71e81
Nikolay <terleew2013@yandex.ru>
2021-12-31 13:41:57 +0000

    feat: create event for basket

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,40 +2,33 @@
 namespace App\EventSubscriber;
 
  use App\Repository\BasketRepository;
- use App\Entity\Basket;
  use App\Repository\ConferenceRepository;
  use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  use Symfony\Component\HttpKernel\Event\ControllerEvent;
  use Twig\Environment;
+ use App\Service\BasketTotalPosition;
 
-
-
-class TwigEventSubscriber implements EventSubscriberInterface
-{
+ class TwigEventSubscriber implements EventSubscriberInterface
+ {
      private $twig;
+     private $totalPosition;
 
-     public function __construct(Environment $twig)
+     public function __construct(Environment $twig, BasketTotalPosition $totalPosition)
      {
-          $this->twig = $twig;
+         $this->twig = $twig;
+         $this->totalPosition = $totalPosition;
      }
 
      public function onKernelController(ControllerEvent $event)
      {
-          
-          $this->twig->addGlobal('count', [
-
-          [
-               'basketsInfo' => 1,
-              
-          ]
-          ]);
+         
+         $this->twig->addGlobal('count', $this->totalPosition->totalBasket());
      }
 
      public static function getSubscribedEvents()
      {
-          return [
+         return [
                'kernel.controller' => 'onKernelController',
           ];
      }
-
-}
+ }
--- src/Service/BasketCreateProduct.php
+++ src/Service/BasketCreateProduct.php
@@ -7,6 +7,8 @@ use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Basket;
 use App\Entity\Product;
+use App\Service\BasketChangeProduct;
+
 
 class BasketCreateProduct
 {
@@ -17,7 +19,7 @@ class BasketCreateProduct
             $product_id = $request->request->get('product_id');
             $price_product= $request->request->get('price_product');
             $product = $doctrine->findOneBy(['id' => $product_id]);
-
+            
             $basket = (new Basket())
                 ->setQuantity($quantity)
                 ->setPriceOne($price_product)
